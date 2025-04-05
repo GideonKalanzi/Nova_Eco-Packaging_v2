d529641609b34453b059d61c621d3ae82a462088
Gideon Kalanzi <gidinsambu@gmail.com>
2025-04-05 11:21:09 +0300

    made a number of changes but now the code is running

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-=======
+
+  
+
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
       extend: {},
     },
     plugins: [],
-  };
+
+};
   
->>>>>>> d4e4925 (Made needed changes)
